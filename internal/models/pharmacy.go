892de2dd3c6c3e8e69014b52f0e86f18863cea20
muslim <muko2muko@yandex.ru>
2025-11-20 13:15:09 +0300

    создал базовый мейн и добавил свою модель

---
+++ internal/models/pharmacy.go
@@ -0,0 +1,18 @@
+package models
+
+import "gorm.io/gorm"
+
+type Pharmacy struct {
+	gorm.Model
+	ID                   int     `json:"id"`
+	Name                 string  `json:"name"`
+	Description          string  `json:"description"`
+	Price                float64 `json:"price"`
+	InStock              bool    `json:"in_stock"`
+	StockQuantity        int     `json:"stock_quantity"`
+	CategoryID           string  `json:"category_id"`
+	SubcategoryID        string  `json:"Subcategory_id"`
+	Manufacturer         string  `json:"manufacturer"`
+	PrescriptionRequired bool    `json:"prescription_required"`
+	AvgRating            float64 `json:"avg_rating"`
+}
