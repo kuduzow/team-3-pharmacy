b9f93a561de264cba8d36da6d79b340e1b8431f4
muslim <muko2muko@yandex.ru>
2025-11-20 16:27:42 +0300

    сделал автомигрейт

--- cmd/pharmacy/main.go
+++ cmd/pharmacy/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"pharmacy-team/internal/config"
+	"pharmacy-team/internal/models"
+)
+
+func main() {
+	db := config.SetUpDatabaseConnection()
+
+	// Выполняем миграции моделей
+	if err := db.AutoMigrate(&models.Payment{}, &models.Pharmacy{}, &models.Review{}, models.User{}); err != nil {
+		log.Fatalf("не удалось выполнить миграции: %v", err)
+	}
+}
