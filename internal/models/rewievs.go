c86c27fe45c29fbecf7ac8b272a463c2596d9086
kuruevimran17 <ivahabov807@gmail.com>
2025-11-23 15:36:39 +0300

    изменил модель отзывов, чтобы использовать float64 для оценок и реализовал проверку отзывов пользователей.

--- internal/models/rewievs.go
+++ internal/models/rewievs.go
@@ -6,18 +6,18 @@ type Review struct {
 	*gorm.Model
 	UserID     uint   `gorm:"not null" json:"user_id"`
 	MedicineID uint   `gorm:"not null" json:"medicine_id"`
-	Rating     int    `gorm:"not null;check:rating>=1 AND rating<=5" json:"rating"`
+	Rating     float64    `gorm:"-" json:"rating"`
 	Text       string `gorm:"type:text" json:"text"`
 }
 
 type CreateReviewRequest struct {
     UserID     uint   `json:"user_id" binding:"required"`
     MedicineID uint   `json:"medicine_id" binding:"required"`
-    Rating     int    `json:"rating" binding:"required,min=1,max=5"`
+    Rating     float64    `json:"rating" binding:"required,min=1,max=5"`
     Text       string `json:"text" binding:"required"`
 }
 
 type UpdateReviewRequest struct {
-    Rating *int   `json:"rating" binding:"omitempty,min=1,max=5"`
+    Rating *float64   `json:"rating" binding:"omitempty,min=1,max=5"`
     Text   *string `json:"text" binding:"omitempty"`
 }
