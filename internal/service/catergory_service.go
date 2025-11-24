99c8762cfccf5e9ac5422806d571609df74b665f
muslim <muko2muko@yandex.ru>
2025-11-24 14:36:10 +0300

    создал транспорт для сабкатегорий и исправил некоторые ошибки

--- internal/service/catergory_service.go
+++ internal/service/catergory_service.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+	"errors"
 	"pharmacy-team/internal/models"
 	"pharmacy-team/internal/repository"
+	"strings"
 )
 
 type CategoryService interface {
 	GetAllCategory() ([]models.Category, error)
+	CreateCategory(req models.CategoryCreateRequest) (*models.Category, error)
 }
 
 type categoryService struct {
@@ -24,4 +27,31 @@ func (c *categoryService) GetAllCategory() ([]models.Category, error) {
 		return nil, err
 	}
 	return category, nil
+}
+
+func (c *categoryService) CreateCategory(req models.CategoryCreateRequest) (*models.Category, error) {
+	if err := c.validateCategoryCreate(req); err != nil {
+		return nil, err
+	}
+	category := &models.Category{
+		ID:   req.ID,
+		Name: req.Name,
+	}
+
+	if err := c.category.Create(category); err != nil {
+		return nil, err
+	}
+	return category, nil
+}
+
+func (c *categoryService) validateCategoryCreate(req models.CategoryCreateRequest) error {
+	if req.ID < 0 {
+		return errors.New("айди не может быть отрицательным")
+	}
+
+	if strings.TrimSpace(req.Name) == "" {
+		return errors.New("имя не может быть пустым")
+	}
+
+	return nil
 }
