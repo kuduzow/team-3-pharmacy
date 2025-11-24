14f050bc79dd36736a24bc4bddb7ce5406064a01
Strannik-chr <vaha.selmurz08@mail.ru>
2025-11-24 12:20:55 +0300

    сделал транспорт payment_hendler

---
+++ internal/transport/payment_hendler.go
@@ -0,0 +1,71 @@
+package transport
+
+import (
+	"net/http"
+	"pharmacy-team/internal/models"
+	"pharmacy-team/internal/service"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type PaymentHandler struct {
+	servicePayment service.PaymentService
+}
+
+func NewPaymentHandler(s service.PaymentService) *PaymentHandler {
+	return &PaymentHandler{s}
+}
+
+func (t *PaymentHandler) RegisterRoutes(r *gin.Engine) {
+	r.POST("/orders/:id/payments", t.CreatePayment)
+	r.GET("/orders/:id/payments", t.GetPaymentsByOrder)
+	r.GET("/payments/:id", t.GetPaymentById)
+}
+
+func (h *PaymentHandler) CreatePayment(c *gin.Context) {
+	orderID, _ := strconv.Atoi(c.Param("id"))
+
+	var req models.PaymentCreate
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	payment, err := h.servicePayment.Create(uint(orderID),req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, payment)
+}
+
+func (h *PaymentHandler) GetPaymentsByOrder(c *gin.Context) {
+	orderID, _ := strconv.Atoi(c.Param("id"))
+
+	payments, err := h.servicePayment.ListByOrder(uint(orderID))
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, payments)
+}
+func (t *PaymentHandler) GetPaymentById(c *gin.Context) {
+    id := c.Param("id")
+
+    paymentID, err := strconv.ParseUint(id, 10, 64)
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "invalid payment id"})
+        return
+    }
+
+    payment, err := t.servicePayment.Get(uint(paymentID))
+    if err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, payment)
+}
