8ac62596977380fd6a8dd0ff3300d86a019668df
kuruevimran17 <ivahabov807@gmail.com>
2025-11-25 22:26:34 +0300

    сделал корзину

--- internal/models/cart.go
+++ internal/models/cart.go
@@ -4,25 +4,30 @@ import "gorm.io/gorm"
 
 type Cart struct {
 	*gorm.Model
-	UserID     uint `json:"user_id"`
-	Items   []CartItem `json:"items" gorm:"foreignKey:CartID"`
-	TotalPrice float64 `json:"total_price"`
+	UserID     uint       `json:"user_id"`
+	Items      []CartItem `json:"items" gorm:"foreignKey:CartID"`
+	TotalPrice float64    `json:"total_price"`
 }
 
 type CartItem struct {
 	*gorm.Model
-	MedicineID uint    `json:"medicine_id"`
-	Quantity   int    `json:"quantity"`
-	Price      float64 `json:"price"`
+	CartID       uint    `json:"cart_id" gorm:"index"`
+	MedicineID   uint    `json:"medicine_id"`
+	Name         string  `json:"name"`
+	Quantity     int     `json:"quantity"`
+	PricePerUnit float64 `json:"price_per_unit"`
 }
 
 type CartCreateRequest struct {
-	UserID     uint          `json:"user_id"`
-	Items   []CartItemRequest `json:"items"`
+	UserID uint              `json:"user_id"`
+	Items  []CartItemRequest `json:"items"`
 }
 
-
 type CartItemRequest struct {
-	MedicineID uint    `json:"medicine_id"`
-	Quantity   int    `json:"quantity"`
-}	
+	MedicineID uint `json:"medicine_id"`
+	Quantity   int  `json:"quantity"`
+}
+
+type CartUpdateRequest struct {
+	Quantity int `json:"quantity"`
+}
